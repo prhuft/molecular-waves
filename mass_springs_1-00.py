5dcd0a52816d4069d99e824eef736e239bce5cfe
Preston Huft <gothrowayo@gmail.com>
2018-05-03 16:24:57 -0500

    rk4_multicoord.py updated to iter over coordinates. Main code in mass_springs_1-00.py finished but with bugs.

--- mass_springs_1-00.py
+++ mass_springs_1-00.py
@@ -17,9 +17,10 @@ of) double_pendulum_1-05.py.
 ## LIBRARIES
 
 import helpfunclib as hfl
-from rk4 import rk4_update as rk4
+from rk4_multicoord import rk4_update as rk4
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
+from random import random as rn
 import numpy as np
 from copy import copy as cp
 import math as m
@@ -30,11 +31,11 @@ from math import sqrt
 ## GLOBAL CONSTANTS
 
 g = 9.8 # [m/s]
-DEBUG = 0 # set to 1 if we're debugging
+DEBUG = 1 # set to 1 if we're debugging
 
 ## METHODS
 
-def derivs(state,params):
+def derivs(state,tau,params):
 	"""Returns a list of the first and second derivatives of the generalized 
 	positions of each mass in the system.
 	
@@ -46,12 +47,19 @@ def derivs(state,params):
 		coordinate1 = [[f1^(0),f2^(0),...,fn^(0)],[f1^(1),f2^(1),...fn^(1)],
 				...,[f1^(m),f2^(m),...fn^(m)]]
 		coordinate2 = likewise, and so on for the other coordinates
+	tau = the timestep. It does not get used here.
 	"""
 	# The mass positions. state[i][j] is the jth deriv group for coord. i
 	x_list = state[0][0]
 	y_list = state[1][0]
 	vx_list = state[0][1]
 	vy_list = state[1][1]
+	
+	if DEBUG:
+		print("x_list ", x_list)
+		print("len(state) ",len(state))
+		print("state[0] ",state[0])
+		print("state[0][0] ",state[0][0])
 	
 	# The spring constant,mass, and network dimensions
 	kx,ky,m,r0,xlen,ylen = params
@@ -70,20 +78,20 @@ def derivs(state,params):
 				n_hj = xlen*(i-1) + j
 				dx = x_list[n_hj]-x_list[n_ij]
 				dy = y_list[n_hj]-y_list[n_ij]
-				ax[n_ij] += o2_x*(1-r0/sqrt(dx**2+dy**2))*dx
+				ax_list[n_ij] = o2_x*(1-r0/sqrt(dx**2+dy**2))*dx
 			if (i < xlen-1): # ... bottom edge
 				n_jj = xlen*(i+1) + j
 				dx = x_list[n_jj]-x_list[n_ij]
 				dy = y_list[n_jj]-y_list[n_ij]
-				ax[n_ij] += o2_x*(1-r0/sqrt(dx**2+dy**2))*dx
+				ax_list[n_ij] = o2_x*(1-r0/sqrt(dx**2+dy**2))*dx
 			if (j > 0): # ... left edge
 				n_ii = xlen*i + (j-1)
 				dx = x_list[n_ii]-x_list[n_ij]
-				ax[n_ij] += -o2_x*(dx+r0)
-			if (j < ylen-1); # ... right edge
-				n_ik = xlen*i + (j+1))
+				ax_list[n_ij] = -o2_x*(dx+r0)
+			if (j < ylen-1): # ... right edge
+				n_ik = xlen*i + (j+1)
 				dx = x_list[n_ik]-x_list[n_ij]
-				ax[n_ij] += o2_x*(dx-r0)
+				ax_list[n_ij] = o2_x*(dx-r0)
 	
 	ay_list = xlen*ylen*[0]
 	for i in range(0,xlen):
@@ -96,91 +104,60 @@ def derivs(state,params):
 				n_ii = xlen*i + (j-1)
 				dx = x_list[n_ii]-x_list[n_ij]
 				dy = y_list[n_ii]-y_list[n_ij]
-				ay[n_ij] += o2_y*(1-r0/sqrt(dx**2+dy**2))*dy
-			if (j < ylen-1); # ... right edge 
-				n_ik = xlen*i + (j+1))
+				ay_list[n_ij] = o2_y*(1-r0/sqrt(dx**2+dy**2))*dy
+			if (j < ylen-1): # ... right edge 
+				n_ik = xlen*i + (j+1)
 				dx = x_list[n_ik]-x_list[n_ij]
 				dy = y_list[n_ik]-y_list[n_ij]
-				ay[n_ij] += o2_y*(1-r0/sqrt(dx**2+dy**2))*dy
+				ay_list[n_ij] = o2_y*(1-r0/sqrt(dx**2+dy**2))*dy
 			if (i > 0): # ... top edge
 				n_hj = xlen*(i-1) + j
 				dy = y_list[n_hj]-y_list[n_ij]
-				ay[n_ij] += -o2_y*(dy+r0)
+				ay_list[n_ij] = -o2_y*(dy+r0)
 			if (i < xlen-1): # ... bottom edge
 				n_jj = xlen*(i+1) + j
 				dy = y_list[n_ik]-y_list[n_ij]
-				ay[n_ij] += o2_y*(dy-r0)
+				ay_list[n_ij] = o2_y*(dy-r0)
 	
 	return [vx_list,ax_list],[vy_list,ay_list]
 		
-# def get_data(state,tau,steps,params,num_update):
-def get_data(states,tau,steps,params,num_update):
-	""" Returns a list of the states of the double pendulum systems at each
-	timestep, for steps number of iterations, dt [s] apart. num_update is the
-	numerical method function to be used. """
-	
-	# Get pendulum parameters; assume same params for each system
-	# m1,m2,l1,l2 = params 
-	
-	# # The arrays of endpoints of arm1 and arm2 for each system. I.e., 
-	# # arm1_data = [[[x1_1,y1_1],[x1_2,y1_2],...,[x1_n,y1_n]],
-	# #				 [[x2_1,y2_1],[x2_2,y2_2],...,[x2_n,y2_n]],...,
-	# #				 [[xm_1,y2_1],[xm_2,y2_2],...,[xm_n,ym_n]]]
-	# # where [xi_j,yi_j] is the endpoint coordinate of arm1 in the ith 
-	# # double pendulum system at step j. Likewise for arm2.
-	# arm1_data,arm2_data = [],[] 
-	
-	# Generate the states at each timestep over the specified number of
-	# steps for each double pendulum system
-	for state in states:
-	
-		# The initial state of the nth double pendulum
-		# t1,t2 = state[0]
-		# o1,o2 = state[1]
-		# a1,a2 = state[2]
+def get_data(state,tau,steps,params,num_update):
+	""" Returns a list of the states of the system at each timestep, for steps
+		number of iterations, tau [s] apart. num_update is thenumerical method 
+		function to be used. 
 		
-		# if DEBUG:
-			# print('Iter 0',': t1,t2= ',t1,t2)
-			# print('o1,o2= ',o1,o2,' a1,a2= ',a1,a2)
-			# print()
+		state = [q1,q2,...,qz] where each q is a necessary parameter for 
+			describing the generalized position of a specific subsystem:
+			q = [[f1^(0),f2^(0),...,fn^(0)],[f1^(1),f2^(1),...fn^(1)],
+				  ...,[f1^(m),f2^(m),...fn^(m)]]
+		params = system-specific constants. E.g., mass, length, density, etc.
+	"""
+	
+	# Extract system parameters
+	kx,ky,m,r0,xlen,ylen = params
 		
-		# Timestep
-		dt = tau
-		
-		# # Initialize the endpoints of each arm, in Cartesian coordinates
-		# xData1,yData1 = [toXY(t1,l1)[0]],[toXY(t1,l1)[1]]
-		# xData2,yData2 = [toXY(t2,l2)[0]+xData1[0]],[toXY(t2,l2)[1]+yData1[0]]
-		
-		# Forward feed the solver method for i = 0 to i = steps
-		for i in range(0,steps): 
-			try:
-				# Update each variable
-				# new_state = num_update([[t1,t2],[o1,o2],[a1,a2]],dt,params,derivs)
-				# t1,t2 = new_state[0]
-				# o1,o2 = new_state[1]
-				# a1,a2 = new_state[2]
-				
-				# xData1 += [toXY(t1,l1)[0]]
-				# yData1 += [toXY(t1,l1)[1]]
-				# xData2 += [toXY(t2,l2)[0]+xData1[i+1]]
-				# yData2 += [toXY(t2,l2)[1]+yData1[i+1]]
-				
-				# if DEBUG:
-					# print('Iter ',i,': t1,t2= ',t1,t2)
-					# print('o1,o2= ',o1,o2,' a1,a2= ',a1,a2)
-					# print()
-				
-			except ValueError:		
-				print('value error at iteration ',i)
-				break
+	# Timestep
+	dt = tau
+	
+	xdata = [] # the x positions
+	ydata = [] # ditto, but in y
+	
+	# Forward feed the solver method for i = 0 to i = steps
+	for i in range(0,steps): 
+		try:
+			# Update the state of the network
+			new_state = num_update(state,dt,params,derivs)
 			
-		# Append the coordinate lists for the nth system  
-		# arm1_data.append([xData1,yData1]) # endpoints of arm 1
-		# arm2_data.append([xData2,yData2]) # endpoints of arm 2
+			xdata.append(new_state[0][0]) 
+			ydata.append(new_state[1][0])
 			
-	return arm1_data,arm2_data
+		except ValueError:		
+			print('value error at iteration ',i)
+			break
+			
+	return xdata,ydata
 	
-def get_initial_state(params):
+def get_initial_state(params,tau):
 	""" Returns a list describing the state, in the form:
 		state = [q1,q2,...,qz] where each q is a necessary parameter for 
 			describing the generalized position of a specific subsystem:
@@ -212,18 +189,20 @@ def get_initial_state(params):
 	state_y = []
 	for i in range(0,xlen): # iterate over the columns
 		for j in range(0,ylen): # iterate over the rows
-			rx_list[xlen*i + (j-1)] = j*ylen + r0*(rn()-.5) # the y coord of mass ij
+			ry_list[xlen*i + (j-1)] = j*ylen + r0*(rn()-.5) # the y coord of mass ij
 	state_y.append(ry_list)
 	state_y.append(vy_list)
 	state_y.append(ay_list)
 		
 	state = [state_x,state_y]
+	print("state: ", state)
 	# Replace ax_list and ay_list using derivs()
-	deriv_list = derivs(state,params)
+	deriv_list = derivs(state,tau,params)
 	state[0][2] = deriv_list[0][1]
 	state[1][2] = deriv_list[1][1]
+	print("state: ", state)
 	
-	return states
+	return state
 	
 ## SYSTEM INITIALIZATION
 
@@ -232,79 +211,44 @@ m = .1 # [kg] these are massive particles lol
 kx = 1 # [N/m] Spring constant in x
 ky = 1 # [N/m] Spring constant in y
 r0 = .1 # [m] the spring equilibrium length
-x_num =  10 # number of columns of masses
-y_num = 10 # number of rows of masses
+x_num =  2 # number of columns of masses
+y_num = 2 # number of rows of masses
 params = [kx,ky,m,r0,x_num,y_num]
-
-# Generate the initial state
-state_0 = get_initial_state(params)
 
 dt = 0.01 # [s]
 iters = 10000 # times to update the systems
 
+# Generate the initial state
+state_0 = get_initial_state(params,dt)
+
 # # Generate the data
-# data = get_data(states_0,dt,iters,params,rk4)
+xdata,ydata = get_data(state_0,dt,iters,params,rk4)
 
 ## SIMULATION SETUP
 
 # Initialize the figure
-# xpts,ypts = [],[]
-# for armdata in data: # iterates twice
-	# for data_n in armdata: # iterate through the data for all n systems
-		# xpts.append(data_n[0])
-		# ypts.append(data_n[1])
-# # xpts and ypts should now each be of length 2n
-# n = int(len(xpts)/2)
-# x1pts,x2pts = xpts[:n],xpts[n:]
-# y1pts,y2pts = ypts[:n],ypts[n:]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_facecolor('black')
+ax.set_aspect(aspect='equal')
+fig.patch.set_facecolor('black')
 
-# if DEBUG: print("pt. lengths: ",len(x1pts),len(x2pts),len(y1pts),len(y2pts))
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.set_facecolor('black')
-# ax.set_aspect(aspect='equal')
-# fig.patch.set_facecolor('black')
-
-# # Initialize the lines to be plotted 
-# pen_lines = []
-# trail1_lines = []
-# trail2_lines = []
-# for k in range(0,total):
-	# trail1_line, = ax.plot([],[],lw=1)
-	# trail2_line, = ax.plot([],[],lw=1)
-	# trail1_lines.append(cp(trail1_line))
-	# trail2_lines.append(cp(trail2_line))
-	# pen_line, = ax.plot([],[],color='white',lw=3)
-	# pen_lines.append(cp(pen_line))
+# # Initialize the lines; actually, it will be more of a scatter plot
+scatter = []
+scatter, = ax.plot([],[],color='purple',marker='o')
 
 def init():
-	""" Set the axes limits. """
-	# l = 0
-	# if (params[2] > params[3]):
-		# l = params[2]
-	# else:
-		# l = params[3]
-
-	# ax.set_ylim(-2*l*1.1,2*l*1.1)
-	# ax.set_xlim(-2*l*1.1,2*l*1.1)
-	# # Concatenate the lists, then return as a tuple
-	return tuple(trail1_lines + trail2_lines + pen_lines)
+	""" Set the axes limits with global values. """
+	ax.set_ylim(-r0,xlen*(r0+1))
+	ax.set_xlim(-r0,ylen*(r0+1))
+	return scatter,
 	
 def update(i):
-	""" Uses values established previously as globals."""
-	# j = i + 1;
-	# # Set the lines to plot
-	# for k in range(0,total):
-		# # The lines from the arm endpoints
-		# trail1_lines[k].set_data(x1pts[k][:j],y1pts[k][:j])
-		# trail2_lines[k].set_data(x2pts[k][:j],y2pts[k][:j])
-		
-		# # The line describing the kth double pendulum
-		# pen_lines[k].set_data([0,x1pts[k][i],x2pts[k][i]],
-							  # [0,y1pts[k][i],y2pts[k][i]])
-	# # Concatenate the lists, then return as a tuple
-	return tuple(trail1_lines + trail2_lines + pen_lines)
+	""" Set the ith data points with global values."""
+	
+	# The points describing the network at the ith step
+	scatter.set_data(xdata[i],ydata[i])
+	return scatter,
 
 # Run the animation
 anim = animation.FuncAnimation(fig, update, frames=range(0,iters+1), 
--- rk4_multicoord.py
+++ rk4_multicoord.py
@@ -21,8 +21,8 @@ DEBUG = 0
 
 ## THE METHOD
 
-def rk4_update(sys_state,h,params,derivatives):
-	""" sys_state = [q1,q2,...,qz] where each q is a necessary parameter for 
+def rk4_update(state,h,params,derivatives):
+	""" state = [q1,q2,...,qz] where each q is a necessary parameter for 
 			describing the generalized position of a specific subsystem:
 			q = [[f1^(0),f2^(0),...,fn^(0)],[f1^(1),f2^(1),...fn^(1)],
 				  ...,[f1^(m),f2^(m),...fn^(m)]]
@@ -36,66 +36,71 @@ def rk4_update(sys_state,h,params,derivatives):
 		derivatives = derivatives(state,h,params)
 			a method which returns a list of the m derivatives of 
 			the generalized position of the n bodies in the system, i.e.:
-			[[f1^(1),f2^(1),...fn^(1)], ...,[f1^(m),f2^(m),...fn^(m)]]
+			[[[fx1^(1),fx2^(1),...,fxn^(1)], ...,[fx1^(m),fx2^(m),...,fxn^(m)]],
+			 [[fy1^(1),fy2^(1),...,fyn^(1)], ...,[fy1^(m),fy2^(m),...,fyn^(m)],
+			 ...] where lists of lists of ordered derivatives are grouped by 
+			 coordinate.
 			
 		This method returns the updated state of the system, in the exact form 
 		of the 'state' list which is passed in. 
 	"""
 	
-	# Run the single-parameter solver of rk4.py for each parameter
-	new_sys_state = []
-	for state in sys_state:
-		def k_mat(dh_mat):
-			""" The output is of the form: 
-				k_mat = [[f1^(0)(q+dh),f2^(0)(q+sh),...,fn^(0)(q+dh)],[f1^(1)(q+dh),
-						f2^(1)(q+dh),...fn^(1)(q+dh)],...,[f1^(m-1)(q+dh),f2^(m-1)(q+dh),
-						...fn^(m-1)(q+h)]]
-						
-				where fi^(j)(q+h) is the jth derivative of the generalized position
-				of the ith object, evaluated at q+h."""
-			k_mat = []
-			for i in range(0,len(state[:-1])): # iter over orders of derivs up to m-1, inclusive
-				k_list = []
-				for j in range(0,len(state[i])): # iter through the object indices
-					temp_s = copy_nested_list(state)
-					temp_s[i][j] = state[i][j]+dh_mat[i][j] # add dh to the ith f^(j)
-					k_list.append(h*derivatives(temp_s,h,params)[i][j])
-				k_mat.append(list(k_list)) # append a copy of l; this is the mth k list
-			return k_mat
+	def k_mat(dh_mat,u): 
+		""" The output is of the form: 
+			k_mat = [[f1^(0)(q+dh),f2^(0)(q+sh),...,fn^(0)(q+dh)],[f1^(1)(q+dh),
+					f2^(1)(q+dh),...fn^(1)(q+dh)],...,[f1^(m-1)(q+dh),f2^(m-1)(q+dh),
+					...fn^(m-1)(q+h)]]
 					
-		def dh_mat(k_mat,c):
-			""" Returns the matrix of steps dh, given the previous matrix of k vals. 
-				The output is in the format of input expected by k_mat(), i.e.:
-				[[h1^1,h2^1,...,hn^1],[h1^2,h2^2,...,hn^2],...,[h1^m,h2^m,...,hn^m]]
+			where fi^(j)(q+h) is the jth derivative of the generalized position
+			of the ith object, evaluated at q+h.
+			u = the current coordinate we're dealing with; the uth coordinate.
+			"""
+		k_mat = []
+		for i in range(0,len(state[u][:-1])): # iter over orders of derivs up to m-1, inclusive
+			k_list = []
+			for j in range(0,len(state[u][i])): # iter through the object indices
+				temp_s = copy_nested_list(state[u])
+				temp_s[i][j] = state[u][i][j]+dh_mat[i][j] # add dh to the ith f^(j)
+				k_list.append(h*derivatives(temp_s,h,params)[u][i][j])
+			k_mat.append(list(k_list)) # append a copy of l; this is the mth k list
+		return k_mat
 				
-				where hi^j = dh is the step in fi^(j)(q+dh)."""
-			dh_mat = []
-			for k_list in k_mat:
-				dh_list = []
-				for k in k_list:
-					dh_list.append(k*c)
-				dh_mat.append(list(dh_list))
-			return dh_mat
+	def dh_mat(k_mat,c):
+		""" Returns the matrix of steps dh, given the previous matrix of k vals. 
+			The output is in the format of input expected by k_mat(), i.e.:
+			[[h1^1,h2^1,...,hn^1],[h1^2,h2^2,...,hn^2],...,[h1^m,h2^m,...,hn^m]]
 			
-		def copy_nested_list(mylist):
-			""" This assumes a depth of 1, i.e. a list of list of non-lists."""
-			copy_list = []
-			for l in mylist:
-				copy_list.append(list(l))
-			return copy_list
+			where hi^j = dh is the step in fi^(j)(q+dh)."""
+		dh_mat = []
+		for k_list in k_mat:
+			dh_list = []
+			for k in k_list:
+				dh_list.append(k*c)
+			dh_mat.append(list(dh_list))
+		return dh_mat
 		
+	def copy_nested_list(mylist):
+		""" This assumes a depth of 1, i.e. a list of list of non-lists."""
+		copy_list = []
+		for l in mylist:
+			copy_list.append(list(l))
+		return copy_list
+		
+	# Run the single-parameter solver of rk4.py for each parameter
+	new_state = []
+	for u in range(0,len(state)):
 		# Create the initial list of zeros to pass into k()
 		dh_mat_0 = []
-		for i in range(0,len(state[:-1])): # up to to m-1, inclusive
-			dh_mat_0.append([0]*len(state[i]))
+		for i in range(0,len(state[u][:-1])): # up to to m-1, inclusive
+			dh_mat_0.append([0]*len(state[u][i]))
 			
 		# Generate the k matrices, where k(dh) def: f(q_i+dh) for systems which do 
 		# not depend explicitly on the parameter (e.g. time) with respect to which 
 		# we are differentiating.
-		k1 = k_mat(dh_mat_0)
-		k2 = k_mat(dh_mat(k1,1/2.))
-		k3 = k_mat(dh_mat(k2,1/2.))
-		k4 = k_mat(k3) # passing in k3 is equivalent to passing in dh_mat(k3,1)
+		k1 = k_mat(dh_mat_0,u)
+		k2 = k_mat(dh_mat(k1,1/2.),u)
+		k3 = k_mat(dh_mat(k2,1/2.),u)
+		k4 = k_mat(k3,u) # passing in k3 is equivalent to passing in dh_mat(k3,1)
 		
 		if DEBUG: 
 			print('dh1: ',dh_mat_0)
@@ -107,16 +112,16 @@ def rk4_update(sys_state,h,params,derivatives):
 			print('dh4: ',k3)
 			print('k4: ',k4)
 		
-		new_state = []
-		for i in range(0,len(state[:-1])): # iter over deriv orders up to m-1, inclusive
+		new_state_u = [] # the part of the state projected on the uth coordinate
+		for i in range(0,len(state[u][:-1])): # iter over deriv orders up to m-1, inclusive
 			new_derivs = []
-			for j in range(0,len(state[0])): # iter over coords up to q_n, inclusive
-				new_derivs.append(state[i][j]+(k1[i][j]+2*(k2[i][j]+k3[i][j])+
+			for j in range(0,len(state[u][0])): # iter over coords up to q_n, inclusive
+				new_derivs.append(state[u][i][j]+(k1[i][j]+2*(k2[i][j]+k3[i][j])+
 									k4[i][j])/6.)
-			new_state.append(list(new_derivs))
+			new_state_u.append(list(new_derivs))
 			
 		# Get the mth derivatives with the analytical method passed in
-		new_state.append(derivatives(state,h,params)[-1])
-		new_sys_state.append(new_state)
+		new_state_u.append(derivatives(state,h,params)[-1])
+		new_state.append(new_state_u)
 		
-	return new_sys_state
+	return new_state
